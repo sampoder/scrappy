2cbcd73f0298229eb83185271109e1bbf1febbd0
Christopher Walker <cwalker@polytrope.com>
2020-06-19 00:31:42 -0400

    change handler priority

--- pages/api/slack/message/index.js
+++ pages/api/slack/message/index.js
@@ -32,10 +32,10 @@ export default async (req, res) => {
     method = 'noFile'
   } else if (event.subtype === 'message_changed' && event.channel == process.env.CHANNEL) {
     method = 'updated'
-  } else if (event.type === 'message' && !event.thread_ts && event.channel === 'C015M6U6JKU') {
-    method = 'css'
   } else if (event.subtype === 'file_share' && !event.thread_ts && event.channel == 'C015M6U6JKU') {
     method = 'cssFile'
+  } else if (event.type === 'message' && !event.thread_ts && event.channel === 'C015M6U6JKU') {
+    method = 'css'
   } else if (event?.message?.text === 'forget scrapbook' && event.channel == process.env.CHANNEL) {
     method = 'forgotten'
   } else {
